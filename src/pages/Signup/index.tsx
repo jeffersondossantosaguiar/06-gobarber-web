5135c698da4ca9a1ed5b4ba7f4ffa3f31fb3cd56
Jefferson <jeffersonsantos.a@gmail.com>
2020-11-16 15:51:11 -0300

    Form

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi'
+import { Form } from '@unform/web'
 
 import logoImg from '../../assets/logo.svg'
 
@@ -8,31 +9,38 @@ import Button from '../../components/Button'
 
 import { Container, Content, Background } from './styles'
 
-const SignUp: React.FC = () => (
-  <Container>
-    <Background />
-    <Content>
-      <img src={logoImg} alt="GoBarber" />
+const SignUp: React.FC = () => {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  function handleSubmit(data: object): void {
+    console.log(data)
+  }
 
-      <form>
-        <h1>Faça o seu cadastro</h1>
-        <Input name="email" icon={FiUser} placeholder="Nome" />
-        <Input name="email" icon={FiMail} placeholder="E-mail" />
-        <Input
-          name="password"
-          icon={FiLock}
-          type="password"
-          placeholder="Senha"
-        />
-        <Button type="submit">Cadastrar</Button>
-      </form>
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
 
-      <a href="abc">
-        <FiArrowLeft />
-        Voltar para logon
-      </a>
-    </Content>
-  </Container>
-)
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça o seu cadastro</h1>
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+
+        <a href="abc">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  )
+}
 
 export default SignUp
