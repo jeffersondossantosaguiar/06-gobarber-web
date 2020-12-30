240f01d5aaacbc2cb1988c51360cdd6918a56356
unknown <jeffersonsantos.a@gmail.com>
2020-12-30 12:28:11 -0300

    Listagem de agendamentos

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { FiClock, FiPower } from 'react-icons/fi'
 import {
@@ -10,12 +10,16 @@ import {
   Schedule,
   NextAppointment,
   Calendar,
+  Section,
+  Appointment,
 } from './styles'
 
 import logoImg from '../../assets/logo.svg'
 import { useAuth } from '../../hooks/auth'
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date())
+
   const { signOut, user } = useAuth()
 
   return (
@@ -62,6 +66,69 @@ const Dashboard: React.FC = () => {
               </span>
             </div>
           </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://tmssl.akamaized.net/images/portrait/originals/52896-1547642704.jpg"
+                  alt="Everton Ribeiro"
+                />
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://tmssl.akamaized.net/images/portrait/originals/52896-1547642704.jpg"
+                  alt="Everton Ribeiro"
+                />
+              </div>
+            </Appointment>
+          </Section>
+          <Section>
+            <strong>Tarde</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://tmssl.akamaized.net/images/portrait/originals/52896-1547642704.jpg"
+                  alt="Everton Ribeiro"
+                />
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://tmssl.akamaized.net/images/portrait/originals/52896-1547642704.jpg"
+                  alt="Everton Ribeiro"
+                />
+              </div>
+            </Appointment>
+          </Section>
         </Schedule>
         <Calendar />
       </Content>
